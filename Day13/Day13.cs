27fd781e39d251b639e4becd03e9428e00f76c30
Willem-Jan van Losser <wvanlosser@rbk.nl>
2020-12-13 09:56:00 +0100

    Day 13 Part Two unfinished

--- Day13/Day13.cs
+++ Day13/Day13.cs
@@ -9,20 +9,29 @@ namespace AdventOfCode2020.Day13
     {
         private long _startTime;
         private List<int> _busses;
+        private Dictionary<int, int> _busOffsets;
 
         public Day13() : base(13, "Shuttle Search")
         {
             _busses = new List<int>();
+            _busOffsets = new Dictionary<int, int>();
         }
 
         protected override void BeforeSolve()
         {
+            var busses = InputLine(1).Split(',');
+         
             _startTime = long.Parse(InputLine(0));
             _busses.Clear();
-            _busses.AddRange(InputLine(1).Split(',')
-                                         .Where(x => x != "x")
-                                         .Select(x => int.Parse(x))
-                                         .ToArray());
+            _busses.AddRange(busses.Where(x => x != "x")
+                                   .Select(x => int.Parse(x))
+                                   .ToArray());
+
+            _busOffsets.Clear();
+            for (int i = 0; i < busses.Length; i++)
+            {
+                if (busses[i] != "x") _busOffsets.Add(int.Parse(busses[i]), i);
+            }
         }
 
         // Correct Answer: 2382
@@ -32,7 +41,7 @@ namespace AdventOfCode2020.Day13
             var earliestBus = 0;
             var earliestTime = _startTime;
 
-            while(!done)
+            while (!done)
             {
                 foreach (var bus in _busses)
                 {
@@ -52,14 +61,62 @@ namespace AdventOfCode2020.Day13
             Console.WriteLine($"Answer: {diffMinutes * earliestBus}");
         }
 
+        protected override void SolvePartTwo()
+        {
+            var matchingBusses = FindMatchingBusses();
+            if (matchingBusses == null) throw new Exception("No matching busses found!");
+
+            var bus = matchingBusses.Item1;
+            var busOffset = _busses.IndexOf(bus);
+
+            var busTest = matchingBusses.Item2;
+            var busTestOffset = _busses.IndexOf(busTest);
+
+            Console.WriteLine("TODO");
+        }
+
         private bool DoesBusDepart(long time, long bus)
         {
             return time % bus == 0;
         }
 
-        protected override void SolvePartTwo()
+        private Tuple<int, int> FindMatchingBusses()
         {
-            Console.WriteLine("TODO");
+            for (int i = 0; i < _busses.Count-1; i++)
+            {
+                var bus = _busses[i];
+                for (int j = i+1; j < _busses.Count; j++)
+                {
+                    var busTest = _busses[j];
+                    if (_busOffsets[busTest] == bus) return new Tuple<int, int>(bus, busTest);
+                }
+
+            }
+
+            return null;
+        }
+
+        private long FindLCM(int bus1, int bus2, long offset = 0)
+        {
+            var bus = bus1 > bus2 ? bus1 : bus2;
+            var busTest = bus1 > bus2 ? bus2 : bus1;
+
+            if (bus % busTest == 0) return busTest;
+
+            var multiplier = offset / bus;
+            var done = false;
+            long lcm = 0;
+
+            while(!done)
+            {
+                multiplier++;
+                long testLCM = bus * multiplier;
+
+                done = testLCM % busTest == 0;
+                if (done) lcm = testLCM;
+            }
+
+            return lcm;
         }
     }
 }
