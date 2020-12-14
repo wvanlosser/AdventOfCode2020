50fd0f77d99c2ad3590b582d0697b086f8d07b11
Willem-Jan van Losser <wvanlosser@rbk.nl>
2020-12-14 21:59:11 +0100

    Day 14 Part One Solved, Part Two failed miserably... :(

--- Day14/Day14.cs
+++ Day14/Day14.cs
@@ -1,6 +1,7 @@
 ﻿using AdventOfCode2020.Shared;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020.Day14
@@ -8,18 +9,21 @@ namespace AdventOfCode2020.Day14
     public class Day14 : DayBase
     {
         private char[] _mask;
-        private long[] _memory;
+        private Dictionary<long, long> _memory;
 
         public Day14() : base(14, "Docking Data")
         {
+            _memory = new Dictionary<long, long>();
         }
 
+        // Correct Answer: 14862056079561
         protected override void SolvePartOne()
         {
             InitMask();
             InitMemory();
+            ProcessLinesVersion1();
 
-            Console.WriteLine("TODO");
+            Console.WriteLine($"Answer: {_memory.Values.Sum()}");
         }
 
         protected override void SolvePartTwo()
@@ -34,38 +38,79 @@ namespace AdventOfCode2020.Day14
         {
             _mask = Enumerable.Repeat('X', 36).ToArray();
         }
-
         private void InitMemory()
         {
-            _memory = Enumerable.Repeat(0L, sizeof(long)).ToArray();
+            _memory.Clear();
+        }
+
+        private void ProcessLinesVersion1()
+        {
+            foreach (var line in Input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    _mask = line.Remove(0, 7).Reverse().ToArray();
+                }
+                else
+                {
+                    var posOB = line.IndexOf("[");
+                    var posCB = line.IndexOf("]");
+
+                    var address = long.Parse(line.Substring(posOB + 1, posCB-posOB-1));
+                    var value = long.Parse(line.Substring(posCB + 3));
+
+                    var bits = ConvertToBits(value);
+                    var bitsWithMask = ApplyBitMask(bits, _mask);
+
+                    _memory[address] = ConvertToValue(bitsWithMask);
+                }
+            }
+        }
+        private void ProcessLinesVersion2()
+        {
+            foreach (var line in Input)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    _mask = line.Remove(0, 7).Reverse().ToArray();
+                }
+                else
+                {
+                    var posOB = line.IndexOf("[");
+                    var posCB = line.IndexOf("]");
+
+                    var address = long.Parse(line.Substring(posOB + 1, posCB - posOB - 1));
+                    var value = long.Parse(line.Substring(posCB + 3));
+
+                    var bits = ConvertToBits(address);
+                    var addresses = DecodeAddresses(bits);
+                    foreach (var addr in addresses)
+                    {
+                        _memory[addr] = value;
+                    }
+                }
+            }
         }
 
         private BitArray ConvertToBits(long value)
         {
             return new BitArray(BitConverter.GetBytes(value));
+            
         }
-
-        private BitArray ApplyBitMask(BitArray bits)
+        private BitArray ApplyBitMask(BitArray bits, char[] mask)
         {
-            var newBits = new BitArray(36);
+            var newBits = new BitArray(bits.Length);
 
-            for (int i = 0; i < bits.Length; i++)
+            for (int i = 0; i < mask.Length; i++)
             {
                 newBits[i] = bits[i];
 
-                if (_mask[i] == '1')
-                {
-                    newBits[i] = true;
-                }
-                else if (_mask[i] == '0')
-                {
-                    newBits[i] = false;
-                }
+                if (mask[i] == '1') newBits[i] = true;
+                if (mask[i] == '0') newBits[i] = false;
             }
 
             return newBits;
         }
-
         private long ConvertToValue(BitArray bits)
         {
             var value = 0L;
@@ -74,6 +119,14 @@ namespace AdventOfCode2020.Day14
                 if (bits[i]) value += (long)Math.Pow(2, i);
             }
             return value;
+        }
+        private List<long> DecodeAddresses(BitArray bits)
+        {
+            var addresses = new List<long>();
+
+            // OUT OF IDEAS..... :(
+
+            return addresses;
         }
     }
 }
